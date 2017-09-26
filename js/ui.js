ce7614c3115d506b68613047ee56be42316128a7
Andrew Tran <tran.andrew.c@gmail.com>
2017-09-26 15:27:22 -0600

    [AT] - Added details support
    Former-commit-id: e3455f984781e384e98ffed4ec1243ebdd6e0a91 [formerly 090ef9223788cb40450bdf6e8e58387bd68e54fa]
    Former-commit-id: 7e7023bf7d8d0eb2b8d8a091a3c4a266274b8952

--- js/ui.js
+++ js/ui.js
@@ -18,6 +18,15 @@ function fetchState() {
     });
 }
 
+function getDescribe(siteName) {
+    var promise = new Promise(function(resolve, reject) {
+        ddevShell.describe(siteName).then(function(data){
+            resolve(data);
+        })
+    });
+    return promise;
+}
+
 function renderUI(list) {
     $('.card-container').empty();
     list.forEach(function(site){
@@ -28,17 +37,55 @@ function renderUI(list) {
 
 function createCard(site){
     var localPath = site.path.replace('~', os.homedir());
-    var markup = `<div class="col-lg-3 col-md-4 col-sm-4 card `+site.state+`" data-path="`+localPath+`">
+    var markup = `<div class="col-lg-3 col-md-4 col-sm-4 card `+site.state+`" data-path="`+localPath+`" data-sitename="`+site.name+`">
         <h2>`+site.name+`</h2>
         <div>`+site.state+`</div>
-        <p><a class="btn btn-secondary" href="#" onclick='electron.shell.openExternal("`+site.url+`")'><i class="fa fa-chrome" aria-hidden="true"></i></a><a class="btn btn-secondary" href='#' onclick='electron.shell.showItemInFolder("`+localPath+`")'><i class="fa fa-folder-open-o" aria-hidden="true"></i></a></p>
+        <p><a class="btn btn-secondary" href="#" onclick='electron.shell.openExternal("`+site.url+`")'><i class="fa fa-chrome" aria-hidden="true"></i></a><a class="btn btn-secondary" href='#' onclick='electron.shell.showItemInFolder("`+localPath+`")'><i class="fa fa-folder-open-o" aria-hidden="true"></i></a><a class="btn btn-secondary infobtn" href='#'><i class="fa fa-info" aria-hidden="true"></i></a></p>
         <p><a class="btn btn-primary startbtn" href="#" role="button"><i class="fa fa-play" aria-hidden="true"></i></a><a class="btn btn-primary stopbtn" href="#" role="button"><i class="fa fa-stop" aria-hidden="true"></i></a><a class="btn btn-primary restartbtn" href="#" role="button"><i class="fa fa-refresh" aria-hidden="true"></i></a><a class="btn btn-primary removebtn" href="#" role="button"><i class="fa fa-trash" aria-hidden="true"></i></a></p>
     </div>`;
 
     return markup;
 }
 
+function createDetails(details){
+    var output = '';
+    for (var key in details) {
+        if (details.hasOwnProperty(key)) {
+            var section = details[key];
+            output += '<div><h3>'+key+'</h3><table>';
+            for (var k in section){
+                if(section.hasOwnProperty(k) && k !== 'notes'){
+                    output += '<tr><td>' + k + '</td>' + '<td>' + section[k] + '</td></tr>';
+                }
+            }
+            output += '</table>';
+            if(section.notes){
+                output += '<ul>';
+                section.notes.forEach(function(note){
+                   output += '<li>' + note + '</li>';
+                });
+                output += '</ul>';
+            }
+            output += '</div>';
+        }
+    }
+    return output;
+}
+
+function updateModal(title, body){
+    $('.modal-title').text(title);
+    $('.modal-body').html(body);
+    $('#ddevModal').modal();
+}
+
 function bindButtons(){
+    $(document).on('click', '.infobtn', function() {
+        console.log('describe');
+        var siteName = $(this).closest('.card').data('sitename');
+        getDescribe(siteName).then(function(data){
+            updateModal('Additional Info For ' + siteName, createDetails(data));
+        });
+    });
     $(document).on('click', '.startbtn', function(){
         console.log('starting');
         ddevShell.start($(this).closest('.card').data('path'), function(data){console.log(data)});
