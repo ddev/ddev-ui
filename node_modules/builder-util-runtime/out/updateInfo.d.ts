1b646fe04622dabc2b9bdc824334f3087f774f6e
Andrew Tran <tran.andrew.c@gmail.com>
2017-09-27 08:54:46 -0600

    [AT] - added linux binary
    Former-commit-id: 5e0b45b77a33f093d643f4d77262c84a9f741119 [formerly cd5775875e9ceed3a27b10f48a4da8fed18b5743]
    Former-commit-id: 0ff133e9a344f0f8fba14ff9bbf4dde35b130657

---
+++ node_modules/builder-util-runtime/out/updateInfo.d.ts
@@ -0,0 +1,43 @@
+export interface VersionInfo {
+    /**
+     * The version.
+     */
+    readonly version: string;
+}
+export interface PackageFileInfo {
+    file: string;
+    size: number;
+    sha512: string;
+    headerSize?: number;
+    blockMapSize?: number;
+    blockMapData?: string;
+}
+export interface UpdateInfo extends VersionInfo {
+    readonly path: string;
+    packages?: {
+        [arch: string]: PackageFileInfo;
+    } | null;
+    githubArtifactName?: string | null;
+    /**
+     * The release name.
+     */
+    readonly releaseName?: string | null;
+    /**
+     * The release notes.
+     */
+    readonly releaseNotes?: string | null;
+    /**
+     * The release date.
+     */
+    readonly releaseDate: string;
+    /**
+     * @deprecated
+     * @private
+     */
+    readonly sha2?: string;
+    readonly sha512?: string;
+    /**
+     * The [staged rollout](auto-update.md#staged-rollouts) percentage, 0-100.
+     */
+    readonly stagingPercentage?: number;
+}
