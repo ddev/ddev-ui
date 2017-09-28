7859ad62db86f13ac821e3c0e5089a83a674425b
Andrew Tran <tran.andrew.c@gmail.com>
2017-09-28 15:53:34 -0600

    [AT] - updated README, added gitignore, removed unnecessary files from repo in preparation for force-push to ddev-ui repo
    Former-commit-id: bc148c895794e7eadeb0969a9709de4dc2443d04

--- node_modules/builder-util/out/api.d.ts
+++
@@ -1,7 +0,0 @@
-import { DebugLogger } from "./DebugLogger";
-export interface PackageBuilder {
-    readonly buildResourcesDir: string;
-    readonly debugLogger: DebugLogger;
-    readonly resourceList: Promise<Array<string>>;
-    getTempFile(suffix: string): Promise<string>;
-}
