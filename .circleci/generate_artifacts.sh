d51e5c39a3772cce0d7e597291338001916977ad
Randy Fay <randy@randyfay.com>
2017-09-29 19:07:24 -0600

    Get proper name for tarballs/zipballs

--- .circleci/generate_artifacts.sh
+++ .circleci/generate_artifacts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -o errexit
+set -x
 
 ARTIFACTS=$1
 BASE_DIR=$PWD
@@ -9,9 +10,12 @@ sudo mkdir $ARTIFACTS && sudo chmod 777 $ARTIFACTS
 export VERSION=$(git describe --tags --always --dirty)
 
 for item in release-builds/DDEV*; do
-	cd $item
-	tar -czf $ARTIFACTS/${item}.tar.gz .
-	zip $ARTIFACTS/${item}.zip .
+	pushd $item
+	filebase=$(basename $item)
+	pwd && ls -a
+	tar -czf $ARTIFACTS/${filebase}.tar.gz .
+	zip $ARTIFACTS/${filebase}.zip *
+	popd
 done
 
 # Create the sha256 files
