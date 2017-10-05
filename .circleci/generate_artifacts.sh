ab7e9fa06cc1edfb47ccc1f4e21fbc44f422d779
Andrew Tran <tran.andrew.c@gmail.com>
2017-10-05 17:07:22 -0600

    Issue 26 build installer binaries (#31)
    * [AT] - added prompt for db remove and added notifications
    
    * [AT] - updated build tools to generate installers (win/darwin) and a single appImage binary (linux)
    
    * [AT] - updated circleCI config to install linux-specific build dependencies.
    
    * Revert "[AT] - added prompt for db remove and added notifications"
    
    This reverts commit 6a8b973ac71e9c8e73934bfa91ffcd686008c2c7 [formerly 69d61f3f33ad3cc43fe592fdeda4c51bb139e198 [formerly 77026657ac7e5a70e1c49f00b6d7c1145db4cf48]].
    
    * pulled commit related to different feature, removed 32bit linux binary
    
    * [AT] - changed mac binary output to zipped .app instead of a .dmg
    
    .dmg can only be built on a mac host, and circleci 2.0 mac support isn't out yet. changing build target to a zipped .app to avoid that step until we can build the .dmg in circle.
    
    * [AT] - trying a different wine installation approach to remedy the ppa being deprecated
    
    * [AT] - trying a different wine installation approach to remedy the ppa being deprecated
    
    * [AT] - trying a different wine installation approach to remedy the ppa being deprecated (ppa-purge method)
    
    * [AT] - trying a different wine installation approach to remedy the ppa being deprecated (ppa-purge of jdk method)
    
    * [AT] - trying a different wine installation approach to remedy the ppa being deprecated (ppa-purge of jdk method)
    
    * [AT] - test commit for ssh
    
    * [AT] - test commit for ssh
    
    * [AT] - removed fonts section and wine installation
    
    * [AT] - removed space from name
    
    * [AT] - fixed typo in package.json
    
    * [AT] - changed windows to be standalone
    
    * Convert build/config.yml to macOS circleci 2.0
    
    * Switch to mac dmg
    
    * Improve installed tool versions stanza on config.yml
    
    * Update package.json.in
    
    * Update Makefile
    
    * Dummy commit whitespace to convince Circleci we're alive
    
    
    Former-commit-id: bb7972844be2ed9aaf415fbd0ced49b2cbd48e93
    Former-commit-id: b0f9a20953300242358fbf2dcc33ea8dd396a689

--- .circleci/generate_artifacts.sh
+++ .circleci/generate_artifacts.sh
@@ -4,21 +4,15 @@ set -o errexit
 set -x
 
 ARTIFACTS=$1
-BASE_DIR=$PWD
 
 sudo mkdir $ARTIFACTS && sudo chmod 777 $ARTIFACTS
-export VERSION=$(git describe --tags --always --dirty)
 
-for item in release-builds/DDEV*; do
-	pushd $item
-	filebase=$(basename $item)
-	tar -czf $ARTIFACTS/${filebase}.$VERSION.tar.gz .
-	zip $ARTIFACTS/${filebase}.$VERSION.zip *
-	popd
-done
+cd dist
+cp ddev-ui* $ARTIFACTS
 
 # Create the sha256 files
 cd $ARTIFACTS
-for item in *.tar.gz *.zip; do
-  sha256sum $item > $item.sha256.txt
+for item in ddev-ui*; do
+  shasum -a 256 "$item" > "$item.sha256.txt"
 done
+
