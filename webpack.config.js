293e060c4dd58b86afb35c30a333df8e412c0a07
Andrew Tran <tran.andrew.c@gmail.com>
2018-02-13 13:48:55 -0700

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/src/ui.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'js/dist')
-    },
-    target: 'electron',
-    /*module: {
-        loaders: [
-            {
+  entry: './js/src/ui.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'js/dist'),
+  },
+  target: 'electron',
+  module: {
+    loaders: [
+      {
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      /* {
                 test: /\.js$/,
                 include: [
                     path.resolve(__dirname, './js/src')
                 ],
                 loader: 'eslint-loader',
                 exclude: /node_modules/
-            },
-        ]
-    },*/
+            }, */
+    ],
+  },
 };
 
