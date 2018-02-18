0437e161ed23945bf9b480731e88e5a3c73c9711
Andrew Tran <tran.andrew.c@gmail.com>
2018-02-18 09:32:47 -0700

    [AT] - initial commit of webpack run

---
+++ js/src/container.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Container extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+        <img alt="ddev logo" src="img/ddev_last.png" className="ddev-logo" />
+      </nav>
+    );
+  }
+}
+
+export default Container;
+
