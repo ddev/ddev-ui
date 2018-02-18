3585f155aa998be3b640de8c695e8351558e4476
Andrew Tran <tran.andrew.c@gmail.com>
2018-02-18 09:29:44 -0700

    [AT] - initial commit of webpack run

---
+++ js/src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Header from './header.jsx';
+import Footer from './footer.jsx';
+
+require('../../scss/main.scss');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
