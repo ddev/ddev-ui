aa69ebf98d08500f447d88a9beb0a23ce1120396
Andrew Tran <tran.andrew.c@gmail.com>
2018-02-21 09:15:50 -0700

    Issue 77 scss refactor (#88)

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/src/ui.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'js/dist')
-    },
-    target: 'electron',
-    /*module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                include: [
-                    path.resolve(__dirname, './js/src')
-                ],
-                loader: 'eslint-loader',
-                exclude: /node_modules/
-            },
-        ]
-    },*/
+  entry: './js/src/ui.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'js/dist'),
+    publicPath: path.resolve(__dirname, './'),
+  },
+  target: 'electron',
+  module: {
+    loaders: [
+      {
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=/fonts/[name].[ext]',
+      },
+    ],
+  },
 };
 
