1640edc70e94b432aced53153a5f4d4e5b16e409
Andrew Tran <tran.andrew.c@gmail.com>
2018-02-22 12:23:02 -0700

    [AT] - Initial react base

--- js/src/container.jsx
+++ js/src/container.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 class Container extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-        <img alt="ddev logo" src="img/ddev_last.png" className="ddev-logo" />
-      </nav>
+      <div>
+        <nav className="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+          <img alt="ddev logo" src="img/ddev_last.png" className="ddev-logo" />
+        </nav>
+        <div className="container">
+          <div className="row card-container" />
+        </div>
+      </div>
     );
   }
 }
