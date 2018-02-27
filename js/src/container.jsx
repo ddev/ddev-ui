4d38afd3b8c8bf59f0429d9592b2b8635f5426b0
Andrew Tran <tran.andrew.c@gmail.com>
2018-02-27 11:57:03 -0700

    [AT] - updated files that were modified during rebase

--- js/src/container.jsx
+++ js/src/container.jsx
@@ -4,10 +4,10 @@ class Container extends React.Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+        <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse">
           <img alt="ddev logo" src="img/ddev_last.png" className="ddev-logo" />
         </nav>
-        <div className="container">
+        <div className="container-fluid">
           <div className="row card-container" />
         </div>
       </div>
