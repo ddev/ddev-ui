4257b4dacbbaaffcbd999e73cd0972706d5dd539
alkymst <webalkymst@gmail.com>
2018-07-11 22:16:57 -0600

    some more cleanup to align with react app

---
+++ src/components/app.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'popper.js';
+import 'bootstrap';
+
+import Header from './Header';
+import Footer from './Footer';
+import Container from './Container';
+
+import { init } from './../ui.js';
+
+window.$ = window.jQuery = require('jquery');
+
+export default class extends React.Component {
+  componentDidMount() {
+    init();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Container />
+        <Footer />
+      </div>
+    );
+  }
+}
