376baaa85c39ac464448cb6ab0c3da6aca7cb20b
alkymst <webalkymst@gmail.com>
2018-08-21 16:15:19 -0600

    catch for projects the app can't find for whatever reason

--- src/renderer/components/ProjectDetail.jsx
+++ src/renderer/components/ProjectDetail.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import ProjectHeader from './ProjectHeader';
 import ProjectInfo from './ProjectInfo';
+import Status from './Status';
 
 class ProjectDetail extends React.PureComponent {
   render() {
@@ -9,23 +10,13 @@ class ProjectDetail extends React.PureComponent {
       <section className="project-info">
         {this.props.project ? (
           <div className="project-details">
-            <div className="loading-overlay">
-              <div>
-                <i className="fa fa-spinner fa-spin loading-spinner" />
-              </div>
-              <div className="loading-text">Working...</div>
-            </div>
-            <div className="error-overlay">
-              <div>
-                <i className="fa fa-exclamation-triangle error-icon" />
-              </div>
-              <div className="error-text">Something Went Wrong</div>
-              <div className="btn btn-primary">OK</div>
-            </div>
+            <Status />
             <ProjectHeader {...this.props.project} />
             <ProjectInfo {...this.props.project} />
           </div>
-        ) : null}
+        ) : (
+          <h1 className="text-center text-danger">Project Was Not Found!</h1>
+        )}
       </section>
     );
   }
