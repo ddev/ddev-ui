d34af5c287b2aa1d674a9220297ccac706f87b88
alkymst <webalkymst@gmail.com>
2018-08-21 10:54:37 -0600

    add a placeholder image for install profiles we don't support yet

--- src/renderer/components/ProjectTypeIcon.jsx
+++ src/renderer/components/ProjectTypeIcon.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import electron from 'electron';
+import fs from 'fs';
 
 // project type Icon
 const ProjectTypeIcon = props => {
   const { type, httpurl } = props;
+  const placeholderImg = require(`${__static}/img/placeholder.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
   if (type) {
-    const platformImg = require(`${__static}/img/${type}.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
-    return (
-      <a
-        href="#!"
-        className="open-site"
-        onClick={e => {
-          e.preventDefault();
-          electron.shell.openExternal(httpurl);
-        }}
-      >
-        <img alt="ddev logo" src={platformImg} className="platform-logo img-fluid" />
-      </a>
-    );
+    const pathToFile = `${__static}/img/${type}.png`; // eslint-disable-line no-undef
+    if (fs.existsSync(pathToFile)) {
+      const platformImg = require(`${__static}/img/${type}.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
+      if (platformImg) {
+        return (
+          <a
+            href="#!"
+            className="open-site"
+            onClick={e => {
+              e.preventDefault();
+              electron.shell.openExternal(httpurl);
+            }}
+          >
+            <img alt="ddev logo" src={platformImg} className="platform-logo img-fluid" />
+          </a>
+        );
+      }
+    } else {
+      return (
+        <a
+          href="#!"
+          className="open-site"
+          onClick={e => {
+            e.preventDefault();
+            electron.shell.openExternal(httpurl);
+          }}
+        >
+          <img alt="ddev logo" src={placeholderImg} className="platform-logo img-fluid" />
+        </a>
+      );
+    }
   }
   return null;
 };
