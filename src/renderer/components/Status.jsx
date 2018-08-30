87047a9f9b7f78dfa84c283fa9a395d91c43908a
alkymst <webalkymst@gmail.com>
2018-08-30 17:59:25 -0600

    break the flex grid

--- src/renderer/components/Status.jsx
+++ src/renderer/components/Status.jsx
@@ -4,13 +4,13 @@ class Status extends React.PureComponent {
   render() {
     return (
       <React.Fragment>
-        <div className="loading-overlay">
+        <div className="loading-overlay flex-column">
           <div>
             <i className="fa fa-spinner fa-spin loading-spinner" />
           </div>
           <div className="loading-text">Working...</div>
         </div>
-        <div className="error-overlay">
+        <div className="error-overlay flex-column">
           <div>
             <i className="fa fa-exclamation-triangle error-icon" />
           </div>
