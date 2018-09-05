c361bd2b88d2267b8df871d2ad856b90f7e5fc2b
alkymst <webalkymst@gmail.com>
2018-09-05 16:47:31 -0600

    update the way we load the cmsType icon

--- src/renderer/components/ProjectTypeIcon.jsx
+++ src/renderer/components/ProjectTypeIcon.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import electron from 'electron';
-import fs from 'fs';
-import * as path from 'path';
 
-global.__static = path.join(__dirname, '/static').replace(/\\/g, '\\\\');
-
-const placeholderImg = require(`${__static}/img/placeholder.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
+const placeholder = require(`${__static}/img/placeholder.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
 const wordpress = require(`${__static}/img/wordpress.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
 const drupal7 = require(`${__static}/img/drupal7.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
 const drupal8 = require(`${__static}/img/drupal8.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
@@ -15,37 +11,22 @@ const php = require(`${__static}/img/php.png`); // eslint-disable-line no-undef,
 const ProjectTypeIcon = props => {
   const { type, httpurl } = props;
   if (type) {
-    const pathToFile = `${__static}/img/${type}.png`; // eslint-disable-line no-undef
-    if (fs.existsSync(pathToFile)) {
-      let platformImg = pathToFile;
-      switch (type) {
-        case 'wordpress':
-          platformImg = wordpress;
-          break;
-        case 'drupal7':
-          platformImg = drupal7;
-          break;
-        case 'drupal8':
-          platformImg = drupal8;
-          break;
-        case 'php':
-          platformImg = php;
-          break;
-        default:
-          break;
-      }
-      return (
-        <a
-          href="#!"
-          className="open-site"
-          onClick={e => {
-            e.preventDefault();
-            electron.shell.openExternal(httpurl);
-          }}
-        >
-          <img alt="ddev logo" src={platformImg} className="platform-logo img-fluid" />
-        </a>
-      );
+    let platformImg = placeholder;
+    switch (type) {
+      case 'wordpress':
+        platformImg = wordpress;
+        break;
+      case 'drupal7':
+        platformImg = drupal7;
+        break;
+      case 'drupal8':
+        platformImg = drupal8;
+        break;
+      case 'php':
+        platformImg = php;
+        break;
+      default:
+        break;
     }
     return (
       <a
@@ -56,11 +37,22 @@ const ProjectTypeIcon = props => {
           electron.shell.openExternal(httpurl);
         }}
       >
-        <img alt="ddev logo" src={placeholderImg} className="platform-logo img-fluid" />
+        <img alt="ddev logo" src={platformImg} className="platform-logo img-fluid" />
       </a>
     );
   }
-  return null;
+  return (
+    <a
+      href="#!"
+      className="open-site"
+      onClick={e => {
+        e.preventDefault();
+        electron.shell.openExternal(httpurl);
+      }}
+    >
+      <img alt="ddev logo" src={placeholder} className="platform-logo img-fluid" />
+    </a>
+  );
 };
 
 export default ProjectTypeIcon;
