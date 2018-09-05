7da5f70172350642c68585a3babb6c15ca46db05
alkymst <webalkymst@gmail.com>
2018-09-05 10:45:26 -0600

    fixed app type icons

--- src/renderer/components/ProjectTypeIcon.jsx
+++ src/renderer/components/ProjectTypeIcon.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import electron from 'electron';
 import fs from 'fs';
 
+const placeholderImg = require(`${__static}/img/placeholder.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
+const wordpress = require(`${__static}/img/wordpress.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
+const drupal7 = require(`${__static}/img/drupal7.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
+const drupal8 = require(`${__static}/img/drupal8.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
+const php = require(`${__static}/img/php.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
+
 // project type Icon
 const ProjectTypeIcon = props => {
   const { type, httpurl } = props;
-  const placeholderImg = require(`${__static}/img/placeholder.png`); // eslint-disable-line no-undef,global-require,import/no-dynamic-require
   if (type) {
     const pathToFile = `${__static}/img/${type}.png`; // eslint-disable-line no-undef
     if (fs.existsSync(pathToFile)) {
