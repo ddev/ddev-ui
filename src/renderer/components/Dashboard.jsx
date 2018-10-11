563804abddc32bd20328e5d42d99469e55fb1043
alkymst <webalkymst@gmail.com>
2018-10-11 09:28:16 -0600

    remove debug info

--- src/renderer/components/Dashboard.jsx
+++ src/renderer/components/Dashboard.jsx
@@ -85,9 +85,6 @@ class Dashboard extends React.Component {
   };
 
   errorCapture = (e, details) => {
-    // console.log(e);
-    // console.log(details);
-
     let error = {};
     if (isJson(e)) {
       error = JSON.parse(e);
