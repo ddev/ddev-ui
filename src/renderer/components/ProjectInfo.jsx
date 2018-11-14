f230cb581680a9b7b47af3d668ada89333c42c83
alkymst <webalkymst@gmail.com>
2018-11-14 15:33:39 -0700

    refactor things to use reactstrap, remove the workaround for bootstrap

--- src/renderer/components/ProjectInfo.jsx
+++ src/renderer/components/ProjectInfo.jsx
@@ -1,21 +1,6 @@
 import React from 'react';
-
-// define the ProjectTab sub-component
-const ProjectTab = props => (
-  <li className="nav-item">
-    <a
-      className={`nav-link ${props.status ? props.status : ''}`}
-      id={`${props.id}-tab`}
-      data-toggle="tab"
-      href={`#${props.id}`}
-      role="tab"
-      aria-controls={props.id}
-      aria-selected="true"
-    >
-      {props.title}
-    </a>
-  </li>
-);
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
 
 // define the ProjectTabItem sub-component
 const ProjectTabItem = props => (
@@ -26,69 +11,117 @@ const ProjectTabItem = props => (
 );
 
 class ProjectInfo extends React.PureComponent {
+  state = {
+    activeTab: '1',
+  };
+
+  toggle = tab => {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab,
+      });
+    }
+  };
+
   render() {
     return (
       <div className="tabs">
-        <ul className="nav nav-tabs" id="myTab" role="tablist">
+        <Nav tabs>
           {/* Project Details */}
-          {this.props.name ? (
-            <ProjectTab title="Project Details" id="site-details" status="active" />
-          ) : null}
+          {this.props.name && (
+            <NavItem>
+              <NavLink
+                className={classnames({ active: this.state.activeTab === '1' })}
+                onClick={() => {
+                  this.toggle('1');
+                }}
+              >
+                Project Details
+              </NavLink>
+            </NavItem>
+          )}
+
           {/* Database */}
-          {this.props.dbinfo ? <ProjectTab title="Database" id="site-db" /> : null}
+          {this.props.dbinfo && (
+            <NavItem>
+              <NavLink
+                className={classnames({ active: this.state.activeTab === '2' })}
+                onClick={() => {
+                  this.toggle('2');
+                }}
+              >
+                Database
+              </NavLink>
+            </NavItem>
+          )}
+
           {/* config */}
-          <ProjectTab title="Config" id="config" />
-        </ul>
-        <div className="tab-content" id="myTabContent">
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '3' })}
+              onClick={() => {
+                this.toggle('3');
+              }}
+            >
+              Config
+            </NavLink>
+          </NavItem>
+        </Nav>
+
+        <TabContent activeTab={this.state.activeTab}>
           {/* Details */}
-          {this.props.name ? (
-            <div
-              className="tab-pane fade show active"
-              id="site-details"
-              role="tabpanel"
-              aria-labelledby="site-details-tab"
-            >
-              <dl className="list-group list-group-flush">
-                <ProjectTabItem title="Name" value={this.props.name} />
-                <ProjectTabItem title="Root" value={this.props.approot} />
-                <ProjectTabItem title="URL" value={this.props.httpurl} />
-                <ProjectTabItem title="PHP Version" value={this.props.php_version} />
-                <ProjectTabItem title="Status" value={this.props.status} />
-                <ProjectTabItem title="Install Type" value={this.props.type} />
-              </dl>
-            </div>
-          ) : null}
+          {this.props.name && (
+            <TabPane tabId="1">
+              <Row>
+                <Col sm="12">
+                  <dl className="list-group list-group-flush">
+                    <ProjectTabItem title="Name" value={this.props.name} />
+                    <ProjectTabItem title="Root" value={this.props.approot} />
+                    <ProjectTabItem title="URL" value={this.props.httpurl} />
+                    <ProjectTabItem title="PHP Version" value={this.props.php_version} />
+                    <ProjectTabItem title="Status" value={this.props.status} />
+                    <ProjectTabItem title="Install Type" value={this.props.type} />
+                  </dl>
+                </Col>
+              </Row>
+            </TabPane>
+          )}
+
           {/* Database */}
-          {this.props.dbinfo ? (
-            <div
-              className="tab-pane fade"
-              id="site-db"
-              role="tabpanel"
-              aria-labelledby="site-db-tab"
-            >
-              <dl className="list-group list-group-flush">
-                <ProjectTabItem title="DB Name" value={this.props.dbinfo.dbname} />
-                <ProjectTabItem title="DB Username" value={this.props.dbinfo.username} />
-                <ProjectTabItem title="DB Host" value={this.props.dbinfo.host} />
-                <ProjectTabItem title="DB Password" value={this.props.dbinfo.password} />
-                <ProjectTabItem title="DB Port" value={this.props.dbinfo.port} />
-                <ProjectTabItem
-                  title="DB Published Port"
-                  value={this.props.dbinfo.published_port}
-                />
-              </dl>
-            </div>
-          ) : null}
+          {this.props.dbinfo && (
+            <TabPane tabId="2">
+              <Row>
+                <Col sm="12">
+                  <dl className="list-group list-group-flush">
+                    <ProjectTabItem title="DB Name" value={this.props.dbinfo.dbname} />
+                    <ProjectTabItem title="DB Username" value={this.props.dbinfo.username} />
+                    <ProjectTabItem title="DB Host" value={this.props.dbinfo.host} />
+                    <ProjectTabItem title="DB Password" value={this.props.dbinfo.password} />
+                    <ProjectTabItem title="DB Port" value={this.props.dbinfo.port} />
+                    <ProjectTabItem
+                      title="DB Published Port"
+                      value={this.props.dbinfo.published_port}
+                    />
+                  </dl>
+                </Col>
+              </Row>
+            </TabPane>
+          )}
+
           {/* config */}
-          <div className="tab-pane fade" id="config" role="tabpanel" aria-labelledby="config-tab">
-            <dl className="list-group list-group-flush">
-              <ProjectTabItem
-                title="WIP"
-                value="This will pull in the config settings from the CLI or reading the config.yml"
-              />
-            </dl>
-          </div>
-        </div>
+          <TabPane tabId="3">
+            <Row>
+              <Col sm="12">
+                <dl className="list-group list-group-flush">
+                  <ProjectTabItem
+                    title="WIP"
+                    value="This will pull in the config settings from the CLI or reading the config.yml"
+                  />
+                </dl>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
       </div>
     );
   }
--- src/renderer/helpers.js
+++ src/renderer/helpers.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import { canReadAndWrite } from './distro-updater';
 import { config, start } from './ddev-shell';
 
